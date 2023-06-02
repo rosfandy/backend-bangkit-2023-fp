518bbe85935c7c984ffc2cabbeca5c29105a8a88
ilhamsakti27 <ilhammuhammadsakti27@gmail.com>
2023-06-02 15:49:22 +0700

    add limiter for predict api

--- src/routes/predict.routes.js
+++ src/routes/predict.routes.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const predictController = require("../controllers/predict")
+const rateLimit = require('express-rate-limit')
+
+const apiLimiter = rateLimit({
+	windowMs: 24 * 60 * 60 * 1000, // 15 minutes
+	max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+// Apply the rate limiting middleware to API calls only
+router.use('/api/predict', apiLimiter)
 
 // Middleware
 const auth = require("../middleware/auth");
