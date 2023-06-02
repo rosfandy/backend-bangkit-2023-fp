009256a9cbd3cd9acdee240ed7276ad5107df009
ilhamsakti27 <ilhammuhammadsakti27@gmail.com>
2023-06-02 20:52:46 +0700

    edit limiter

--- src/routes/predict.routes.js
+++ src/routes/predict.routes.js
@@ -4,8 +4,18 @@ const predictController = require("../controllers/predict")
 const rateLimit = require('express-rate-limit')
 
 const apiLimiter = rateLimit({
-	windowMs: 24 * 60 * 60 * 1000, // 15 minutes
-	max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	windowMs: 60 * 60 * 1000, // 15 minutes
+	max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	message: async (req, res) => {
+		try {
+			res.send({ success: true, message: "You can only make 10 requests every hour.", status:429 });
+		}
+		catch (e) {
+			console.log(error);
+    		res.status(500).send({ success: false, error: "Internal Server Error", status:500 });
+		}
+	},
+	// message: 'Toko tutup woi',
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
